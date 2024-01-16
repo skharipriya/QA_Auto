243d15d015952e5ebeae3332c8676058613482b0
skharipriya <153832106+skharipriya@users.noreply.github.com>
2024-01-16 18:49:53 +0530

    Update store.java

--- src/main/java/TCs/store.java
+++ src/main/java/TCs/store.java
@@ -5,11 +5,16 @@ public class store {
 		String sname;
 		String address;
 		String contact;
+	 ArrayList<Product> products;
 
 		store(String sname, String address, String contact) {
 			this.sname = sname;
 			this.address = address;
 			this.contact = contact;
+			this.products = new ArrayList<Product>();
+		}
+		public void addProd(Product e) {
+			this.products.add(e);
 		}
 	}
 
